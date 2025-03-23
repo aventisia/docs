ace77fb71610c298674b22f79f0744402df0e3b5
gaurav <gaurav@aventisia.com>
2025-03-23 23:00:15 +0530

    Configure Nextra content directory base path and enable default copy code display

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import nextra from "nextra";
 
 const withNextra = nextra({
+  // contentDirBasePath: The base path for your content directory. As per nextra docs, if folder name is "content", you can omit this but getting error if not provided.
+  contentDirBasePath: "/", 
+  defaultShowCopyCode: true,
   // ... Other Nextra config options
 });
 
