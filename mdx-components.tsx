c00700fbb1abf5cd7f36b94f42238f8526c462eb
gaurav <gaurav@aventisia.com>
2025-03-23 22:59:53 +0530

    Add useMDXComponents function to merge custom MDX components with theme components

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,11 +1,15 @@
+// nextra-theme-blog or your custom theme
 import { useMDXComponents as getThemeComponents } from "nextra-theme-docs";
-import { MDXComponents } from "nextra/mdx-components";
 
 // Get the default MDX components
 const themeComponents = getThemeComponents();
 
-// Merge components
-export function useMDXComponents(components: MDXComponents) {
+/**
+ * Merge components
+ * @param components The MDX components
+ * @returns The merged components
+ */
+export function useMDXComponents(components = {}) {
   return {
     ...themeComponents,
     ...components,
