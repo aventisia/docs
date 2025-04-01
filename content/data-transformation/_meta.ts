c5a253781c9277b1560bbb974e333e9d3891bcab
ashish11 <ashish@aventisia.com>
2025-04-01 09:10:41 +0000

    refactor: Folder structure changed. #6

---
+++ content/data-transformation/_meta.ts
@@ -0,0 +1,22 @@
+import type { MetaRecord } from "nextra";
+
+const meta: MetaRecord = {
+  "cryptography": {
+    title: "Cryptography",
+  },
+  "database": {
+    title: "Database",
+  },
+  "datatable": {
+    title: "Data Table",
+  },
+  "date-time": {
+    title: "Date Time",
+  },
+  "text-action": {
+    title: "Text Action",
+  },
+};
+
+export default meta;
+
