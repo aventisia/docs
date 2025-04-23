208d9da16d0491456ee54bb627766514060b3e16
Pooja-Bagul <bagul.pooja995@gmail.com>
2025-04-23 19:21:20 +0530

    Agent Builder folder updated

---
+++ content/agent-builder/_meta.ts
@@ -0,0 +1,36 @@
+import type { MetaRecord } from "nextra";
+
+const meta: MetaRecord = {
+    "index": {
+        title: "Getting Started with Agent",
+        theme: {
+        breadcrumb: false,
+        },
+ },
+  "create-agent": {
+    title: "Create Agent",
+    theme: {
+      breadcrumb: false,
+    },
+  },
+    "navigating-agent-builder": {
+        title: "Agent Builder",
+        theme: {
+        breadcrumb: false,
+        },
+    },
+    "troubleshooting": {
+        title: "troubleshooting Agent Builder",
+        theme: {
+        breadcrumb: false,
+        },
+    },
+    "variables-anotation": {
+        title: "Variable Anotation",
+        theme: {
+        breadcrumb: false,
+        },
+    },
+};
+
+export default meta;
