992cb97494f64a4f6ca4916dca0215b312dfda1f
1224kaushal <kaushal1224@gmail.com>
2025-06-12 20:31:57 +0530

    File: Meta file changes

--- content/agent-builder/ui-interface-automations/file/_meta.ts
+++ content/agent-builder/ui-interface-automations/file/_meta.ts
@@ -1,51 +1,51 @@
 import type { MetaRecord } from "nextra";
 
 const meta: MetaRecord = {
-  "if-file-exists": {
-    title: "If File Exists",
-  },
-  "wait-for-file": {
-    title: "Wait for File",
-  },
-  "copy-files": {
-    title: "Copy Files",
-  },
-  "rename-files": {
-    title: "Rename Files",
-  },
-  "move-files": {
-    title: "Move Files",
-  },
-  "delete-files": {
-    title: "Delete Files",
-  },
-  "read-text-from-file": {
-    title: "Read Text from File",
-  },
-  "write-text-to-file": {
-    title: "Write Text to File",
-  },
-  "read-from-csv-file": {
-    title: "Read from CSV File",
-  },
-  "write-to-csv-file": {
-    title: "Write to CSV File",
-  },
-  "get-file-path-part": {
-    title: "Get File Path Part",
-  },
-  "get-temporary-file": {
-    title: "Get Temporary File",
-  },
   "convert-base64-to-text": {
     title: "Convert Base64 to Text",
-  },
-  "convert-file-to-binary-data": {
-    title: "Convert File to Binary Data",
   },
   "convert-binary-data-to-file": {
     title: "Convert Binary Data to File",
   },
+  "convert-file-to-binary-data": {
+    title: "Convert File to Binary Data",
+  },
+  "copy-files": {
+    title: "Copy Files",
+  },
+  "delete-files": {
+    title: "Delete Files",
+  },
+   "get-file-path-components": {
+    title: "Get File Path Components",
+  },
+  "get-temporary-file": {
+    title: "Get Temporary File",
+  },
+  "if-file-exists": {
+    title: "If File Exists",
+  },
+  "move-files": {
+    title: "Move Files",
+  },
+  "read-from-csv-file": {
+    title: "Read from CSV File",
+  },
+  "read-text-from-file": {
+    title: "Read Text from File",
+  },
+  "rename-files": {
+    title: "Rename Files",
+  },
+  "write-text-to-file": {
+    title: "Write Text to File",
+  },
+  "write-to-csv-file": {
+    title: "Write to CSV File",
+  },
+  "wait-for-file": {
+    title: "Wait for File",
+  }
 
 };
 
