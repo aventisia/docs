39ed42a4a71b8436124b338e8b15dfdc54d94d8b
1224kaushal <kaushal1224@gmail.com>
2025-06-12 19:51:46 +0530

    Folders: metafile changes

--- content/agent-builder/ui-interface-automations/folder/_meta.ts
+++ content/agent-builder/ui-interface-automations/folder/_meta.ts
@@ -2,7 +2,34 @@ import type { MetaRecord } from "nextra";
 
 const meta: MetaRecord = {
   "copy-folder": {
-    title: "Copy Folder",
+    title: "Folder to Copy",
+  },
+  "create-folder": {
+    title: "Create Folder",
+  },
+  "delete-folder": {
+    title: "Delete Folder",
+  },
+  "empty-folder": {
+    title: "Empty Folder",
+  },
+  "get-files-in-folder": {
+    title: "Retrieve Files in Folder",
+  },
+  "get-special-folder-path": {
+    title: "Get Special Folder Path",
+  },
+  "get-subfolders-in-folder": {
+    title: "Retrieve Folders from Directory",
+  },
+  "if-folder-exists": {
+    title: "If Folder Exists",
+  }, 
+  "move-folder": {
+    title: "Move Folder",
+  },
+  "rename-folder": {
+    title: "Rename Folder",
   }
 };
 
