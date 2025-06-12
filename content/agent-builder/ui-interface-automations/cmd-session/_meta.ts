7047fc4e5881347ef851ec6afa6cd6f5d64eb2de
1224kaushal <kaushal1224@gmail.com>
2025-06-12 20:36:04 +0530

    CMD session: meta file updates

--- content/agent-builder/ui-interface-automations/cmd-session/_meta.ts
+++ content/agent-builder/ui-interface-automations/cmd-session/_meta.ts
@@ -2,7 +2,19 @@ import type { MetaRecord } from "nextra";
 
 const meta: MetaRecord = {
   "close-cmd-session": {
-    title: "close-cmd-session",
+    title: "Close CMD Session",
+  },
+  "open-cmd-session": {
+    title: "Open CMD Session",
+  },
+  "read-from-cmd-session": {
+    title: "Read from CMD Session",
+  },
+  "wait-for-text-on-cmd-session": {
+    title: "Wait for Text on CMD Session",
+  },
+  "write-to-cmd-session": {
+    title: "Write to CMD Session",
   }
 };
 
