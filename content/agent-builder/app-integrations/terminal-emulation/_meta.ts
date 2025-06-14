ce35b4c10b43560b7a24c52c57f8ff35cc82a5e3
1224kaushal <kaushal1224@gmail.com>
2025-06-14 13:21:31 +0530

    Terminal Emulation: metafile changes

--- content/agent-builder/app-integrations/terminal-emulation/_meta.ts
+++ content/agent-builder/app-integrations/terminal-emulation/_meta.ts
@@ -3,6 +3,27 @@ import type { MetaRecord } from "nextra";
 const meta: MetaRecord = {
   "close-terminal-session": {
     title: "Close Terminal Session",
+  },
+  "get-text-from-terminal-session": {
+    title: "Get Text from Terminal Session",
+  },
+  "move-cursor-on-terminal-session": {
+    title: "Move Cursor on Terminal Session",
+  },
+  "open-terminal-session": {
+    title: "Open Terminal Session",
+  },
+  "search-for-text-on-terminal-session": {
+    title: "Search for Text on Terminal Session",
+  },
+  "send-key-to-terminal-session": {
+    title: "Send Key to Terminal Session",
+  },
+  "set-text-on-terminal-session": {
+    title: "Set Text on Terminal Session",
+  },
+  "wait-for-text-on-terminal-session": {
+    title: "Wait for Text on Terminal Session",
   }
 };
 
