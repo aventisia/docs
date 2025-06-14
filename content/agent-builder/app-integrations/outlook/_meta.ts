141aaca2cc16b006d8860827a0ed632184bdf4ca
1224kaushal <kaushal1224@gmail.com>
2025-06-14 13:26:20 +0530

    Outlook: metafile changes

--- content/agent-builder/app-integrations/outlook/_meta.ts
+++ content/agent-builder/app-integrations/outlook/_meta.ts
@@ -3,6 +3,18 @@ import type { MetaRecord } from "nextra";
 const meta: MetaRecord = {
   "launch-outlook": {
     title: "Launch Outlook",
+  },
+  "close-outlook": {
+    title: "Close Outlook",
+  },
+  "process-email-messages-in-outlook": {
+    title: "Process Email Messages in Outlook",
+  },
+  "retrieve-email-messages-from-outlook": {
+    title: "Retrieve Email Message from Outlook",
+  },
+  "save-outlook-email-messages": {
+    title: "Save Outlook Email Messages",
   }
 };
 
