1db3573889d575428656c307db0324320ebe4b0c
1224kaushal <kaushal1224@gmail.com>
2025-06-14 13:35:28 +0530

    FTP: metafile changes

--- content/agent-builder/app-integrations/ftp/_meta.ts
+++ content/agent-builder/app-integrations/ftp/_meta.ts
@@ -3,6 +3,39 @@ import type { MetaRecord } from "nextra";
 const meta: MetaRecord = {
   "change-working-directory": {
     title: "Change Working Directory",
+  },
+  "close-connection": {
+    title: "Close Connection",
+  },
+  "create-ftp-directory": {
+    title: "Create FTP Directory",
+  },
+  "delete-ftp-directory": {
+    title: "Delete FTP Directory",
+  },
+  "download-file(s)-from-ftp": {
+    title: "Download File(s) from FTP",
+  },
+  "invoke-ftp-command": {
+    title: "Invoke FTP Command",
+  },
+  "list-ftp-directory": {
+    title: "List FTP Directory",
+  },
+  "open-ftp-connection": {
+    title: "Open FTP Connection",
+  },
+  "open-secure-ftp-connection": {
+    title: "Open Secure FTP Connection",
+  },
+  "rename-ftp-file": {
+    title: "Rename FTP File",
+  },
+  "synchronize-directories": {
+    title: "Synchrinize Directories",
+  },
+  "upload-file(s)-to-ftp": {
+    title: "Upload File(s) to FTP",
   }
 };
 
