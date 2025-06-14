d95d9494a27a3beb9f7467ca081b1a5f118624b0
1224kaushal <kaushal1224@gmail.com>
2025-06-14 13:27:57 +0530

    HTTP: metafile changes

--- content/agent-builder/app-integrations/http/_meta.ts
+++ content/agent-builder/app-integrations/http/_meta.ts
@@ -3,6 +3,9 @@ import type { MetaRecord } from "nextra";
 const meta: MetaRecord = {
   "download-from-web": {
     title: "Download From Web",
+  },
+  "invoke-web-service": {
+    title: "Invoke SOAP Web Serviceß",
   }
 };
 
