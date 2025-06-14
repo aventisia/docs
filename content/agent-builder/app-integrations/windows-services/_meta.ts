b7d685b2f9707aa777124c647a3418238dd77b65
1224kaushal <kaushal1224@gmail.com>
2025-06-14 13:18:10 +0530

    Windows Service: metafile changes

--- content/agent-builder/app-integrations/windows-services/_meta.ts
+++ content/agent-builder/app-integrations/windows-services/_meta.ts
@@ -3,6 +3,21 @@ import type { MetaRecord } from "nextra";
 const meta: MetaRecord = {
   "if-service": {
     title: "If Service",
+  },
+  "pause-service": {
+    title: "Pause Service",
+  },
+  "resume-service": {
+    title: "Resume Service",
+  },
+  "start-service": {
+    title: "Start Service",
+  },
+  "stop-service": {
+    title: "Stop Service",
+  },
+  "wait-for-service": {
+    title: "Wait for Service",
   }
 };
 
