fd8a69f9b12f30431d7db7476301d1ab0250457b
1224kaushal <kaushal1224@gmail.com>
2025-06-14 13:41:14 +0530

    Exchange Server: metafile changes

--- content/agent-builder/app-integrations/exchange-server/_meta.ts
+++ content/agent-builder/app-integrations/exchange-server/_meta.ts
@@ -3,6 +3,15 @@ import type { MetaRecord } from "nextra";
 const meta: MetaRecord = {
   "connect-to-exchange-server": {
     title: "Connect to Exchange Server",
+  },
+  "process-exchange-email-messages": {
+    title: "Process Exchange Email Messages",
+  },
+  "retrieve-exchange-email-messages": {
+    title: "Retrieve Exchange Email Messages",
+  },
+  "send-exchange-email-messages": {
+    title: "Send Exchange Email Messages",
   }
 };
 
