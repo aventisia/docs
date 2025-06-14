553c6dada5bb88c25fd498cb6f93e8a262218a37
1224kaushal <kaushal1224@gmail.com>
2025-06-14 13:42:28 +0530

    Email: metafile updates

--- content/agent-builder/app-integrations/email/_meta.ts
+++ content/agent-builder/app-integrations/email/_meta.ts
@@ -3,6 +3,12 @@ import type { MetaRecord } from "nextra";
 const meta: MetaRecord = {
   "process-email-messages": {
     title: "Process Email Messages",
+  },
+  "retrieve-email-messages": {
+    title: "Retrieve Email Messages",
+  },
+  "send-email": {
+    title: "Send Email",
   }
 };
 
