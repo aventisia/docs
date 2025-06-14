5bec582ff3dd10b30f5e230af17de8b126f3a4d5
1224kaushal <kaushal1224@gmail.com>
2025-06-14 13:22:52 +0530

    PDF: metafile changes

--- content/agent-builder/app-integrations/pdf/_meta.ts
+++ content/agent-builder/app-integrations/pdf/_meta.ts
@@ -3,6 +3,9 @@ import type { MetaRecord } from "nextra";
 const meta: MetaRecord = {
   "extract-images-from-pdf": {
     title: "Extract Images from Pdf",
+  },
+  "extract-tables-from-pdf": {
+    title: "Extract Tables from Pdf",
   }
 };
 
