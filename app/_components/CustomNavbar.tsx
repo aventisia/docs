0fabe9b7cc5942d30c75d9897aae74f9a94f5ad9
bvaibhav23 <vaibhavbhaskar23@gmail.com>
2025-09-23 13:56:06 +0530

    feat: Add Library menu item to navbar

--- app/_components/CustomNavbar.tsx
+++ app/_components/CustomNavbar.tsx
@@ -1,6 +1,13 @@
 import { Navbar } from "nextra-theme-docs";
 import { AppLogo } from "./AppLogo";
+import Link from "next/link";
 
 export default function CustomNavbar() {
-  return <Navbar logo={<AppLogo />} />;
+  return (
+    <Navbar logo={<AppLogo />}>
+      <Link href="/library">
+        <span className="nx-mx-4 nx-font-medium">Library</span>
+      </Link>
+    </Navbar>
+  );
 }
