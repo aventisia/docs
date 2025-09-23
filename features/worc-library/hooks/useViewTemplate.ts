ad9b50179c866210757911168a7253699639a355
bvaibhav23 <vaibhavbhaskar23@gmail.com>
2025-09-23 14:20:48 +0530

    feat: Implement hooks to view and clone templates

---
+++ features/worc-library/hooks/useViewTemplate.ts
@@ -0,0 +1,17 @@
+"use client";
+/**
+ * Custom hook to view a template.
+ *
+ * The template is displayed at the `/library` route.
+ *
+ * @returns A function that takes a template ID and opens the template in a new tab.
+ */
+export const useViewTemplate = () => {
+  // Function to open the template
+  const handleViewTemplate = (id: string) => {
+    const url = `/library/${id}`;
+    window.open(url, "_blank");
+  };
+
+  return handleViewTemplate;
+};
